12ba6fb2db017844299baecd164bbaa757ad3ecf
Duncan kariuki <duncankariuki60@gmail.com>
2022-05-29 22:41:48 +0300

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Duncan K on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
